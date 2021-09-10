ee440510d7b686f313eed97776f05f6dfd08df60
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-09-09 22:34:13 -0500

    Initializing Variables

--- mad-libs.py
+++ mad-libs.py
@@ -1,17 +1,24 @@
 # Init Variables
-
+theMatrix = ""
+system = ""
+neo = ""
+enemy = ""
+inside = ""
+save = ""
+unplugged = ""
+fight = ""
 # Get User Input
 
 # Init Story
 madlibsStory = (
-    "The Matrix is a system, Neo. That system is our enemy. " +
-    "But when you're inside, you look around, what do you see? " +
-    "Businessmen, teachers, lawyers, carpenters. The very minds " +
-    "of the people we are trying to save. But until we do, " +
-    "these people are still a part of that system and that makes " +
-    "them our enemy. You have to understand, most of these people " +
-    "are not ready to be unplugged. And many of them are so inured, " +
-    "so hopelessly dependent on the system, that they will fight to protect it."
+    f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. " +
+    f"But when you're {inside}, you look around, what do you see? " +
+    f"Businessmen, teachers, lawyers, carpenters. The very minds " +
+    f"of the people we are trying to {save}. But until we do, " +
+    f"these people are still a part of that {system} and that makes " +
+    f"them our {enemy}. You have to understand, most of these people " +
+    f"are not ready to be {unplugged}. And many of them are so inured, " +
+    f"so hopelessly dependent on the {system}, that they will {fight} to protect it."
 )
 # Print Story
 print(madlibsStory)
