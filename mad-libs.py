73339305133583640ef1e3cae0aed387880040a6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-09-09 22:37:06 -0500

    Initializing variables

--- mad-libs.py
+++ mad-libs.py
@@ -7,18 +7,23 @@ inside = ""
 save = ""
 unplugged = ""
 fight = ""
+
+profession = ["", "", "", ""]
+adj = ["", ""]
+
 # Get User Input
+
 
 # Init Story
 madlibsStory = (
     f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. " +
     f"But when you're {inside}, you look around, what do you see? " +
-    f"Businessmen, teachers, lawyers, carpenters. The very minds " +
+    f"{profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds " +
     f"of the people we are trying to {save}. But until we do, " +
     f"these people are still a part of that {system} and that makes " +
     f"them our {enemy}. You have to understand, most of these people " +
-    f"are not ready to be {unplugged}. And many of them are so inured, " +
-    f"so hopelessly dependent on the {system}, that they will {fight} to protect it."
+    f"are not ready to be {unplugged}. And many of them are so {adj[0]}, " +
+    f"so hopelessly {adj[1]} on the {system}, that they will {fight} to protect it."
 )
 # Print Story
 print(madlibsStory)
