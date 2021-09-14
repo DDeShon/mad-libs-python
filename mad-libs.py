65dc9ab3f0676f3e605fc170214d48c4d55f7f52
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-09-13 22:56:47 -0500

    fixing typo

--- mad-libs.py
+++ mad-libs.py
@@ -18,19 +18,19 @@ neo = input("Please enter your name.\n")
 
 # Get theMatrix variable from user
 print(f"Hello {neo}! Let's beign.")
-theMatrix = input("What is something you would like to know more about?")
+theMatrix = input("What is something you would like to know more about?\n")
 
 # Get system variable from user
 print(f"So you want to know more about {theMatrix}, do you?")
 print(f"First, tell me what you already know about {theMatrix}.")
-system = input(f"What noun would you best use to describe {theMatrix}?")
+system = input(f"What noun would you best use to describe {theMatrix}?\n")
 
 # Get enemy variable from user
 enemy = input(
-    f"Please provide me with another noun, in opposition to {system}.")
+    f"Please provide me with another noun, in opposition to {system}.\n")
 
 # Get inside variable from user
-inside = input(f"Please tell me your favorite place to relax.")
+inside = input(f"Please tell me your favorite place to relax.\n")
 
 # Get profession list from user
 print(f"What are the top 4 jobs that you would most like to have?")
@@ -40,10 +40,10 @@ for i in range(len(profession)):
 
 # Get save variable from user
 save = input(
-    f"Now, please provide me with a verb to provide action for our story.")
+    f"Now, please provide me with a verb to provide action for our story.\n")
 
 # Get unplugged variable from user
-unplugged = input(f"We're nearing the end now. Please give me another verb.")
+unplugged = input(f"We're nearing the end now. Please give me another verb.\n")
 
 # Get adjectives from user
 print(f"Please provide me with 2 descriptive adjectives.")
@@ -53,11 +53,11 @@ for i in range(len(adj)):
 
 # Get fight variable from user
 fight = input(
-    f"The end is finally here. Please provide one final verb, to wrap up the story.")
+    f"The end is finally here. Please provide one final verb, to wrap up the story.\n")
 
 # Init Story
 madlibsStory = (
-    f"{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. " +
+    f"\n\n{theMatrix} is a {system}, {neo}. That {system} is our {enemy}. " +
     f"But when you're {inside}, you look around, what do you see? " +
     f"{profession[0]}, {profession[1]}, {profession[2]}, {profession[3]}. The very minds " +
     f"of the people we are trying to {save}. But until we do, " +
